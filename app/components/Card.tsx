5305bcf66adcf6cf030cddf2c3a7d1a4e747a897
JenilKyada04 <jenilkyada007@gmail.com>
2026-01-07 19:23:45 +0530

    Enhance Card component layout and styling; remove unused font variables from globals.css

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,19 +1,28 @@
 
+import { IoMdArrowRoundForward } from "react-icons/io";
+
 function Card() {
     return (
-        <div className="white-blue p-2  " >
-            <div className="m-2 border-black grid grid-cols-3" >
-                <div>
-                    <h5>UI Design: Zero to Mastery</h5>
-                    <p>David Hartmann</p>
+        <div className=" p-2 grid md:grid-cols-3 gap-2   " >
+            <div className=" white-blue p-4 border-2 rounded-3xl">
+                <h5 className="font-semibold" >UI Design: Zero to Mastery</h5>
+                <p className="font-normal"> David Hartmann</p>
+                <div className="flex font-light text-gray-600 text-[12px] gap-3 ">
+                    <div>
+                        <p>23/30 Lesson</p>
+                        <p>615 Students</p>
+                    </div>
+                    <div>
+                        <p>77/110 Hours</p>
+                        <p>2/3 Exams </p>
+                    </div>
                 </div>
-                <div>
-                    <h5>UI Design: Zero to Mastery</h5>
-                    <p>David Hartmann</p>
-                </div>
-                <div>
-                    <h5>UI Design: Zero to Mastery</h5>
-                    <p>David Hartmann</p>
+                <div className="flex justify-between">
+                    <p>level : <span className="pl-1" >Intermediate</span></p>
+                    <p className="text-blue-500 cursor-pointer flex items-center">
+                        <p>Continue</p>
+                        <IoMdArrowRoundForward />
+                    </p>
                 </div>
             </div>
         </div>
