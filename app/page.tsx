da23b09424787b37574e4c469962985cf7bf0d79
JenilKyada04 <jenilkyada007@gmail.com>
2026-01-18 18:01:29 +0530

    feat: add suspense loading for dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Linechart } from "../component/Linechart";
 import { Barchart } from "../component/Barchart";
@@ -9,59 +9,58 @@ import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from "@/components/ui/tooltip";
 
-export default function DashboardPage() {
+export default function DashboardClient() {
   return (
-    <>
-      <div className="md:p-6 p-3">
+    <div className="md:p-6 p-3">
+      <Tooltip>
+        <TooltipTrigger className="text-xl font-semibold mt-3 md:mt-0 cursor-pointer">
+          Hello, Antonio
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Hello, Antonio</p>
+        </TooltipContent>
+      </Tooltip>
 
-        <Tooltip>
-          <TooltipTrigger className="text-xl font-semibold mt-3 md:mt-0 cursor-pointer" >Hello, Antonio</TooltipTrigger>
-          <TooltipContent>
-            <p>Hello, Antonio</p>
-          </TooltipContent>
-        </Tooltip>
+      <div className="mt-6 pt-4 bg-white relative rounded-lg shadow">
+        <span className="absolute left-0 top-4 h-6 w-1 bg-blue-600 rounded-full" />
 
-        <div className="mt-6 pt-4  bg-white relative rounded-lg shadow">
+        <div className="flex justify-between">
+          <h6 className="pl-5 font-semibold text-xl cursor-pointer">
+            On going courses
+            <span
+              className="text-gray-400 pl-5 cursor-pointer"
+              onClick={() => alert("DONE")}
+            >
+              Completed Courses
+            </span>
+          </h6>
 
-          <span className="absolute left-0 top-4 h-6 w-1 bg-blue-600 rounded-full" />
-
-          <div className="flex justify-between ">
-            <h6 className="pl-5 font-semibold text-xl cursor-pointer">On going courses
-              <span className="text-gray-400 pl-5 cursor-pointer" onClick={()=>{alert("DONE")}} >Completed Courses</span>
-            </h6>
-
-            <h6 className='text-blue-600 cursor-pointer pr-4'>See All</h6>
-          </div>
-
-          <div className="p-2" >
-            <Card />
-          </div>
+          <h6 className="text-blue-600 cursor-pointer pr-4">See All</h6>
         </div>
 
-
-
-        <div className="grid grid-cols-1 lg:grid-cols-12 mt-4 gap-4">
-          <div className="lg:col-span-8">
-            <Barchart />
-          </div>
-
-          <div className="lg:col-span-4">
-            <Reminders />
-          </div>
+        <div className="p-2">
+          <Card />
         </div>
-
-
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
-          <div className="lg:col-span-2">
-            <Linechart />
-          </div>
-          <Topteachers />
-        </div>
-
       </div>
 
-    </>
+      <div className="grid grid-cols-1 lg:grid-cols-12 mt-4 gap-4">
+        <div className="lg:col-span-8">
+          <Barchart />
+        </div>
+
+        <div className="lg:col-span-4">
+          <Reminders />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4">
+        <div className="lg:col-span-2">
+          <Linechart />
+        </div>
+        <Topteachers />
+      </div>
+    </div>
   );
 }
--- component/DashboardLayout.tsx
+++ component/DashboardLayout.tsx
@@ -1,33 +1,32 @@
 "use client";
 
+import { Suspense } from "react";
 import Sidebar from "./Sidebar";
 import Footer from "./Footer";
 import Header from "./Navbar";
 import React from "react";
 
-
-function DashboardLayout({
+export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen overflow-hidden">
-      <Sidebar />
+    <Suspense fallback={<div className="p-6">Loading dashboard...</div>}>
+      <div className="flex h-screen overflow-hidden">
+        <Sidebar />
 
-      <div className="flex-1 ml-0 md:ml-64 flex flex-col">
-        <header className="fixed top-0 left-0 md:left-64 right-0 h-16 bg-white z-40 border-b">
-          <Header />
-        </header>
+        <div className="flex-1 ml-0 md:ml-64 flex flex-col">
+          <header className="fixed top-0 left-0 md:left-64 right-0 h-16 bg-white z-40 border-b">
+            <Header />
+          </header>
 
-        <main className="mt-16 flex-1 overflow-y-auto ">
-           {children}
-
-          <Footer />
-        </main>
+          <main className="mt-16 flex-1 overflow-y-auto">
+            {children}
+            <Footer />
+          </main>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 }
-
-export default DashboardLayout;
